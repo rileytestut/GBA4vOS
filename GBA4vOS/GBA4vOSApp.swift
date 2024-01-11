2c9aae48dfb1c6f979d3a3aaa13af881a9d92203
Riley Testut <riley@rileytestut.com>
2024-01-11 14:48:32 -0600

    Initial Commit

---
+++ GBA4vOS/GBA4vOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBA4vOSApp.swift
+//  GBA4vOS
+//
+//  Created by Riley Testut on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GBA4vOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
